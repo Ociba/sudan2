96e9190cd8a2ff147a037045e76ba01a0bd3eb0e
Ociba James <ocibajames@gmail.com>
2021-10-07 21:54:27 +0300

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -211,7 +211,7 @@
 <section id="chartjs-bar-charts">
     <!-- Column Chart -->
     <div class="row">
-        <div class="col-12">
+        <div class="col-6">
             <div class="card">
                 <div class="card-header">
                     <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
@@ -228,6 +228,28 @@
                     <div class="card-body">
                             <div class="height-400">
                         <canvas id="column-chart"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+        <div class="card">
+                <div class="card-header">
+                    <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                    <div class="heading-elements">
+                        <ul class="list-inline mb-0">
+                            <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                            <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                            <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                            <li><a data-action="close"><i class="ft-x"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="card-content collapse show">
+                    <div class="card-body chartjs">
+                            <div class="height-400">
+                        <canvas id="line-chart"></canvas>
                         </div>
                     </div>
                 </div>
@@ -318,5 +340,6 @@
      <script src="{{ asset('admin/app-assets/js/scripts/charts/chartjs/pie-doughnut/pie-simple.min.js')}}" type="text/javascript"></script>
      <script src="{{ asset('admin/app-assets/js/scripts/charts/chartjs/pie-doughnut/doughnut-simple.min.js')}}" type="text/javascript"></script>
      <script src="{{ asset('admin/app-assets/js/scripts/charts/chartjs/pie-doughnut/pie-simple2.min.js')}}" type="text/javascript"></script>
+     <script src="{{ asset('admin/app-assets/js/scripts/charts/chartjs/line/line.min.js')}}" type="text/javascript"></script>
   </body>
 </html>
